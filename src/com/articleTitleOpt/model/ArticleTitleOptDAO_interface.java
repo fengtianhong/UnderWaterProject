7c587d9627f5ad67411b040c7ddc36b590560071
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2021-05-30 23:47:12 +0800

    articleTitleOpt*3 0530v1

--- src/com/articleTitleOpt/model/ArticleTitleOptDAO_interface.java
+++ src/com/articleTitleOpt/model/ArticleTitleOptDAO_interface.java
@@ -6,7 +6,7 @@ public interface ArticleTitleOptDAO_interface {
 	public void insert(ArticleTitleOptVO articleTitleOptVO);
     public void update(ArticleTitleOptVO articleTitleOptVO);
     public ArticleTitleOptVO findByPrimaryKey(Integer articleTitleOptSN);
-    public List<ArticleTitleOptVO> getAll();   
+    public List<ArticleTitleOptVO> getAll();   //查詢所有標題
 //	查詢所有文章標題(1對多，回傳set)
 //	萬用複合查詢(傳入參數型態Map)(回傳 List)
 //  public List<ArticleTitleOptVO> getAll(Map<String, String[]> map); 
