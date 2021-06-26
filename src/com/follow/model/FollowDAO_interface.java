484228bee94b74ed7bc4b42ea71e27ccf7118b8f
fengtianhong <4a090136@stust.edu.tw>
2021-06-26 22:36:52 +0800

    friend add

--- src/com/follow/model/FollowDAO_interface.java
+++ src/com/follow/model/FollowDAO_interface.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 
 public interface FollowDAO_interface {
-	public void insert(FollowVO FollowVO);
+	public void insert(Integer follower,Integer followed);
     public void delete(Integer follower,Integer followed);
-    public List<FollowVO> findFollower(Integer follower);//我追蹤誰
-    public List<FollowVO> findFollowed(Integer followed);//誰追蹤我
+    public List<Integer> findFollower(Integer follower);//我追蹤誰
+    public List<Integer> findFollowed(Integer followed);//誰追蹤我
 
 }
