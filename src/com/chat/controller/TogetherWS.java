974afb7b849191f5694c783f541cb480271c6859
fengtianhong <4a090136@stust.edu.tw>
2021-07-05 11:00:48 +0800

    群聊上線啦

---
+++ src/com/chat/controller/TogetherWS.java
@@ -0,0 +1,57 @@
+package com.chat.controller;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/TogetherWS/{userName}")//後面可自己取名byF 對應27行的opOpen用 可在後面增加成{roomID}
+
+//@ServerEndpoint("/TogetherWS/{userName}"/{roomID})
+public class TogetherWS {
+	private static final Set<Session> connectedSessions = Collections.synchronizedSet(new HashSet<>());
+
+	/*
+	 * 如果想取得HttpSession與ServletContext必須實作
+	 * ServerEndpointConfig.Configurator.modifyHandshake()，
+	 * 參考https://stackoverflow.com/questions/21888425/accessing-servletcontext-and-httpsession-in-onmessage-of-a-jsr-356-serverendpoint
+	 */
+	@OnOpen //OnOpen 下方的onOpen可自己命名  當連線建立(開啟)的時候 類似init
+	public void onOpen(@PathParam("userName") String userName, Session userSession) throws IOException {
+		connectedSessions.add(userSession);
+		String text = String.format("Session ID = %s, connected; userName = %s", userSession.getId(), userName);
+		System.out.println(text);
+	}
+
+	@OnMessage //當server接收到資料的時候
+	public void onMessage(Session userSession, String message) {
+		for (Session session : connectedSessions) {
+			if (session.isOpen())
+				session.getAsyncRemote().sendText(message);
+		}
+		System.out.println("Message received: " + message);
+	}
+
+	@OnClose //當連線結束(斷線)的時候
+	public void onClose(Session userSession, CloseReason reason) {
+		connectedSessions.remove(userSession);
+		String text = String.format("session ID = %s, disconnected; close code = %d; reason phrase = %s",
+				userSession.getId(), reason.getCloseCode().getCode(), reason.getReasonPhrase());
+		System.out.println(text);
+	}
+
+	@OnError //當發生例外的時候
+	public void onError(Session userSession, Throwable e) {
+		System.out.println("Error: " + e.toString());
+	}
+
+}
